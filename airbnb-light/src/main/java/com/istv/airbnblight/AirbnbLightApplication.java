2ca45b140e14dc5eea8c68eb2749e96f1307489a
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2019-10-14 13:08:56 +0200

    Ajout du projet spring-boot

---
+++ airbnb-light/src/main/java/com/istv/airbnblight/AirbnbLightApplication.java
@@ -0,0 +1,13 @@
+package com.istv.airbnblight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbLightApplication.class, args);
+	}
+
+}
---
+++ airbnb-light/src/test/java/com/istv/airbnblight/AirbnbLightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.istv.airbnblight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirbnbLightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
