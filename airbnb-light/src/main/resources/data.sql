ed96d1be1789c9a616960536b57b0b03bb665a00
Sadgasmification <jourdain.nicolas1997@gmail.com>
2019-10-17 13:49:14 +0200

    Update data.sql

--- airbnb-light/src/main/resources/data.sql
+++ airbnb-light/src/main/resources/data.sql
@@ -1,3 +1,15 @@
+INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `password`) VALUES
+(1,	'1@test.fr',	'Nicolas',	'Jourdain',	'$2a$10$dEDwmfzBAKkmpkaTLRBy1ef5QwLNg3ujkkH5LKLhEXbuA4myzW0Zu');
+
+INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `password`) VALUES
+(2,	'2@test.fr',	'Aurélien',	'Pietrzak',	'$2a$10$dEDwmfzBAKkmpkaTLRBy1ef5QwLNg3ujkkH5LKLhEXbuA4myzW0Zu');
+
+INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `password`) VALUES
+(3,	'3@test.fr',	'Mikael',	'Desertot',	'$2a$10$dEDwmfzBAKkmpkaTLRBy1ef5QwLNg3ujkkH5LKLhEXbuA4myzW0Zu');
+
+INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `password`) VALUES
+(4,	'4@test.fr',	'Emmanuel',	'Adam',	'$2a$10$dEDwmfzBAKkmpkaTLRBy1ef5QwLNg3ujkkH5LKLhEXbuA4myzW0Zu');
+
 insert
   into
     accommodation
