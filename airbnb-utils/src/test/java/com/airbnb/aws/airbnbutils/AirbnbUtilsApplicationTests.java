56a93742e785f77e57a996ff08e91a409744ca20
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2019-12-02 22:05:21 +0100

    add air-bnb utils

---
+++ airbnb-utils/src/test/java/com/airbnb/aws/airbnbutils/AirbnbUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.aws.airbnbutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
